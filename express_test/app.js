1aca03eb82bc04fd689e79e535761219094cc66f
southernman <alexweber7373@gmail.com>
2023-08-25 15:34:05 -0700

    made both flask server and express js server generate login tokens

--- express_test/app.js
+++ express_test/app.js
@@ -1,21 +1,28 @@
 const express = require('express')
+const bodyparser = require('body-parser')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 
 const port = 3000
 
+const secretkey = "12345"
 
 // middleware
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
-
+app.use(bodyparser.json())
 
 app.post('/post', (req, res) => {
 
   console.log("Data:", req.body)
 
-  res.send("POST request received")
+  const username = req.body.username
+
+  const token = jwt.sign({ username }, secretkey, { expiresIn: '1h' })
+
+  res.json({ token })
 })
 
 app.listen(port, () => {
--- flask_test/app.py
+++ flask_test/app.py
@@ -1,1 +1,20 @@
-from flask import request
+from flask import Flask, request, jsonify
+import jwt
+import datetime
+
+app = Flask(__name__)
+
+secret_key = "12345"
+
+@app.route('/post', methods=['POST'])
+def post():
+  username = request.json.get('username')
+  
+  token = jwt.encode({'username': username}, secret_key, algorithm="HS256")
+  
+  print(f"Got the username {username}")
+  
+  return jsonify({'token': token})
+  
+if __name__ == '__main__':
+  app.run(host='127.0.0.1', port=3000)
